d78a3daad36b3b778f827db3ef46a2f71c781e45
Seong Cho <axelcho@gmail.com>
2013-11-10 19:42:56 -0500

    page.js

--- routes/page.js
+++ routes/page.js
@@ -1,14 +1,20 @@
+var PostsDAO = require('../posts').PostsDAO
+  , sanitize = require('validator').sanitize; // Helper to sanitize form input
+
 /* The ContentHandler must be constructed with a connected db */
 function PageHandler (db) {
     "use strict";
 
+	var posts = new PostsDAO(db);
+	
     this.displayPage = function(req, res, next) {
         "use strict";
 	var page = req.params.page;
 
-
         return res.render(page, {
             title: page,
+			username: req.username,
+            myposts: results
         });
     }
 
