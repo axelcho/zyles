5112bd98ef642d89b72c1a55691fbbc41e99e530
Seong Cho <axelcho@gmail.com>
2013-12-16 21:27:59 -0500

    fixed double braces

--- routes/content.js
+++ routes/content.js
@@ -20,8 +20,7 @@ function ContentHandler (db) {
                 title: 'Vocabulary',				
                 username: req.username,
                 item: results
-            });
-            });
+            });           
         });
 		
 	
