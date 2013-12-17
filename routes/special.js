b95b41d2ffa31c8e82a4e3efc6392981053d1e43
Seong Cho <axelcho@gmail.com>
2013-12-16 20:41:01 -0500

    kill special

--- routes/special.js
+++ routes/special.js
@@ -1,24 +1,23 @@
-var VocabDAO = require('../vocab').VocabDAO
-  , sanitize = require('validator').sanitize; // Helper to sanitize form input
+//var VocabDAO = require('../vocab').VocabDAO; // Helper to sanitize form input
 
 /* The ContentHandler must be constructed with a connected db */
 function SpecialHandler (db) {
     "use strict";
 
-	var vocab = new VocabDAO(db);
+	//var vocab = new VocabDAO(db);
 	
     this.displayVocabularay = function(req, res, next) {
-        "use strict";		
-	        vocab.getByRandom(function(err, results) {
-            "use strict";
+        //"use strict";		
+	    //    vocab.getByRandom(function(err, results) {
+        //    "use strict";
 
-            if (err) return next(err);
+        //    if (err) return next(err);
 
             return res.render('vocabulary', {
                 title: 'Vocabulary',				
                 username: req.username,
-                item: results
-            });
+          //      item: results
+        //    });
         });
     }
 
