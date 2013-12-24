562a324946c27eec02112cafa73740c0b7b67356
Seong Cho <axelcho@gmail.com>
2013-12-24 14:44:11 -0500

    ??

--- vocab.js
+++ vocab.js
@@ -9,12 +9,27 @@ function VocabDAO(db) {
 	var vocab = db.collection("vocabularay"); 	
 	
 	this.getVocab = function(callback){
+<<<<<<< HEAD
 		"use strict";		
 		vocab.count(function(err, num){
 			if (err) return callback(err, null);		
 		
 			var random = Math.floor(Math.random()*num);
 			console.log(random);
+=======
+		"use strict";
+		//console.log("calling vocabulary");
+		
+		db.collection("vocabulary").count(function(err, num){
+		if (err) return callback(err, null);		
+		
+		//console.log(num); 
+		
+		var random = Math.floor(Math.random()*num);
+		
+		console.log(random);
+
+>>>>>>> parent of 125cddd... vocab
 		
 			vocab.findOne({ '_id' : random }, function(err, voc) {
 				"use strict";
