d915d98d56399b161ecfff80b8c40f87f6ca2fd5
Seong Cho <axelcho@gmail.com>
2013-12-24 15:48:18 -0500

    console log

--- vocab.js
+++ vocab.js
@@ -49,15 +49,16 @@ function VocabDAO(db) {
 				output.word = voc[answer].word;
 				output.part = voc[answer].part;
 				
-				var definition = "";
+				var def = "";
 				for (var k = 1; k< 5; k++)
 				{
 				var newline = k + ") " + voc[k-1].definition + "<br/>";
-				definition.concat(newline);
+				def.concat(newline);
 				}
+				console.log(def);
 				
-				output.definition = definition;
-				output.answer = answer; 
+				output.definition = def;
+				output.answer = answer +1; 
 				
 				
 			
